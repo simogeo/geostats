ee56cf6d82840743d5927cc323055bdd1b842d36
simogeo <simon@linea21.com>
2013-02-05 15:08:56 +0100

    update samples with getUniqueValues()

--- tests/openlayers_sample.js
+++ tests/openlayers_sample.js
@@ -9,6 +9,14 @@ function getClass(val, a) {
     }
 }
 
+function getClassUniqueValues(val, a) {
+	// return 2;
+    for(var i= 0; i < a.length; i++)
+    {	
+        if(val == a[i]) {return i;}
+    }
+}
+
 // Function called from body tag
 function init(color_x, class_x) {
 
@@ -66,5 +74,65 @@ function init(color_x, class_x) {
     map.addControl(new OpenLayers.Control.Navigation());
     map.addControl(new OpenLayers.Control.PanZoomBar());
     map.addControl(new OpenLayers.Control.ScaleLine({geodesic: true, maxWidth: 200, bottomOutUnits: "", bottomInUnits: ""}));
+
+}
+
+//Function called from body tag
+function initUniqueValues(color_x, class_x) {
+
+    var lon = 6.3;
+    var lat = 48.73;
+    var zoom = 4;
+
+    OpenLayers.ImgPath = "http://js.mapbox.com/theme/dark/";
+    
+    var context_x = {
+        getColour: function(feature) {
+    	color = color_x;
+    	return color[getClassUniqueValues(feature.attributes["value"], class_x)];
+        }
+    };    
+
+    var template = {
+		fillOpacity: 0.9,
+        strokeColor: "#ffffff",
+        strokeWidth: 1,
+        fillColor: "${getColour}"
+    };
+
+    // Style
+    var style_x = new OpenLayers.Style(template, {context: context_x});
+    var styleMap_x = new OpenLayers.StyleMap({'default': style_x});
+    
+    // options de la carte
+	var options = {
+		scales: [10000000, 5000000, 2500000, 1000000, 500000, 250000],
+        controls: [],  // Remove all controls
+        // allOverlays: true
+    };
+
+    // Create a new map with options defined above
+    map2 = new OpenLayers.Map( 'map2', options );
+    
+
+    // Create polygon layer as vector features
+    // http://dev.openlayers.org/docs/files/OpenLayers/Layer/Vector-js.html
+
+    layer_x = new OpenLayers.Layer.GML("choropleth", "tests/test.json",
+                { format: OpenLayers.Format.GeoJSON,
+    			styleMap: styleMap_x,
+	            isBaseLayer: true,
+	            projection: new OpenLayers.Projection("EPSG:4326")
+	           });
+
+    map2.addLayers([layer_x]);
+    map2.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+    
+    // Add map controls:
+	// http://dev.openlayers.org/docs/files/OpenLayers/Control-js.html
+    map2.addControl(new OpenLayers.Control.MousePosition());
+    map2.addControl(new OpenLayers.Control.Navigation());
+    map2.addControl(new OpenLayers.Control.PanZoomBar());
+    map2.addControl(new OpenLayers.Control.ScaleLine({geodesic: true, maxWidth: 200, bottomOutUnits: "", bottomInUnits: ""}));
 
 }
