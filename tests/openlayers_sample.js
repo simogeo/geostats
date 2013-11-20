0bf913f664116ced29e64adb54d43c1445bc06d5
simogeo <simon@linea21.com>
2013-11-20 16:23:52 +0100

    adding chroma example

--- tests/openlayers_sample.js
+++ tests/openlayers_sample.js
@@ -81,6 +81,66 @@ function init(color_x, class_x) {
 }
 
 //Function called from body tag
+function initChromaSample(color_x, class_x) {
+
+    var lon = 6.3;
+    var lat = 48.73;
+    var zoom = 4;
+
+    OpenLayers.ImgPath = "http://js.mapbox.com/theme/dark/";
+    
+    var context_x = {
+        getColour: function(feature) {
+    	color = color_x;
+    	return color[getClass(feature.attributes["Pop_2006"], class_x)];
+        }
+    };    
+
+    var template = {
+		fillOpacity: 0.9,
+        strokeColor: "#ffffff",
+        strokeWidth: 1,
+        fillColor: "${getColour}"
+    };
+
+    // Style
+    var style_x = new OpenLayers.Style(template, {context: context_x});
+    var styleMap_x = new OpenLayers.StyleMap({'default': style_x});
+    
+    // options de la carte
+	var options = {
+		scales: [10000000, 5000000, 2500000, 1000000, 500000, 250000],
+        controls: [],  // Remove all controls
+        // allOverlays: true
+    };
+
+    // Create a new map with options defined above
+    map = new OpenLayers.Map( 'map3', options );
+    
+
+    // Create polygon layer as vector features
+    // http://dev.openlayers.org/docs/files/OpenLayers/Layer/Vector-js.html
+
+    layer_x = new OpenLayers.Layer.GML("choropleth", "tests/test.json",
+                { format: OpenLayers.Format.GeoJSON,
+    			styleMap: styleMap_x,
+	            isBaseLayer: true,
+	            projection: new OpenLayers.Projection("EPSG:4326")
+	           });
+
+    map.addLayers([layer_x]);
+    map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+    
+    // Add map controls:
+	// http://dev.openlayers.org/docs/files/OpenLayers/Control-js.html
+    map.addControl(new OpenLayers.Control.MousePosition());
+    map.addControl(new OpenLayers.Control.Navigation());
+    map.addControl(new OpenLayers.Control.PanZoomBar());
+    map.addControl(new OpenLayers.Control.ScaleLine({geodesic: true, maxWidth: 200, bottomOutUnits: "", bottomInUnits: ""}));
+
+}
+
+//Function called from body tag
 function initUniqueValues(color_x, class_x) {
 
     var lon = 6.3;
